fe3b461f2c7ab6cf49769ae683e5225499b64a7a
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-11-30 15:28:03 +0100

    feat: add nix flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          pico-sdk-with-submodules = pkgs.pico-sdk.override {
+            withSubmodules = true;
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              cmake
+              gcc-arm-embedded
+              picotool
+              pico-sdk-with-submodules
+              clang
+              clang-tools
+            ];
+
+            PICO_SDK_PATH = "${pico-sdk-with-submodules}/lib/pico-sdk";
+          };
+        });
+    };
+}
