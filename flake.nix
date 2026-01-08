777ca867aa418bd092e28dea22ff50de83c6f892
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-01-08 17:46:03 +0000

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -1,39 +1,38 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05?shallow=1";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    in
-    {
-      devShells = forAllSystems (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          pico-sdk-with-submodules = pkgs.pico-sdk.override {
-            withSubmodules = true;
-          };
-        in
-        {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              pkg-config
-              cmake
-              gcc-arm-embedded
-              picotool
-              pico-sdk-with-submodules
-              clang
-              clang-tools
-              picocom
-            ];
-            PICO_SDK_PATH = "${pico-sdk-with-submodules}/lib/pico-sdk";
-          };
-        });
-    };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      pico-sdk-with-submodules = pkgs.pico-sdk.override {
+        withSubmodules = true;
+      };
+    in {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          pkg-config
+          cmake
+          gcc-arm-embedded
+          picotool
+          pico-sdk-with-submodules
+          clang
+          clang-tools
+          picocom
+        ];
+        PICO_SDK_PATH = "${pico-sdk-with-submodules}/lib/pico-sdk";
+      };
+    });
+  };
 }
